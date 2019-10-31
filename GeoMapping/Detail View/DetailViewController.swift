a295db8ee7605733f4e6699e95472c4bf34688c6
Paul Hise <paulhise1@gmail.com>
2019-10-30 23:46:12 -0700

    Change DetailView to ViewController

--- GeoMapping/Detail View/DetailViewController.swift
+++ GeoMapping/Detail View/DetailViewController.swift
@@ -1,9 +1,9 @@
 import UIKit
 
-class DetailView: UIView {
+class DetailViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
-    @IBOutlet weak var LatitudeLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
     @IBOutlet weak var longitudeLabel: UILabel!
     
     func configure(title: String, message: String, latitude: String, longitude: String) {
