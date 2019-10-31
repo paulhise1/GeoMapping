668dc841970a8db6f7a13b1e12876abcc417e428
Paul Hise <paulhise1@gmail.com>
2019-10-30 21:53:53 -0700

    Add LocationAPIErrors enum

---
+++ GeoMapping/Services/LocationAPIErrors.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+import Foundation
+
+enum LocationAPIError: Error, LocalizedError {
+    case urlError(URLError)
+    case responseError(Int)
+    case decodingError(DecodingError)
+    case genericError
+    
+    var localizedDescription: String {
+        switch self {
+        case .urlError(let error):
+            return error.localizedDescription
+        case .decodingError(let error):
+            return error.localizedDescription
+        case .responseError(let status):
+            return "Bad response code: \(status)"
+        case .genericError:
+            return "An unknown error has been occured"
+        }
+    }
+}
