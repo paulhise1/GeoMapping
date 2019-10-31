4ed814e802962bc14ff3fb013c9688cd23fbb599
Paul Hise <paulhise1@gmail.com>
2019-10-30 23:29:45 -0700

    Create DetailView

---
+++ GeoMapping/Detail View/DetailView.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class DetailView: UIView {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var LatitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+}
