6282fc69c8cc10f35605db88f492fabd9ef96d50
Paul Hise <paulhise1@gmail.com>
2019-11-01 21:24:03 -0700

    Changes some of the image urls.

--- GeoMapping/Location/LocationImageURLDataStore.swift
+++ GeoMapping/Location/LocationImageURLDataStore.swift
@@ -4,6 +4,6 @@ struct LocationImageURLDataStore {
     static let urls: [Int: URL?] = [1: URL(string: "https://media-cdn.tripadvisor.com/media/photo-s/0f/f3/43/4b/photo0jpg.jpg"),
                                     2: URL(string:"https://i.pinimg.com/originals/e3/59/6e/e3596ed6b6f4b19b51b6d6393b845774.jpg"),
                                     3: URL(string:"https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvcGxhY2VfaW1hZ2VzL2U4cXI3b2xuY3A1ZTg2ZWJhZjE2NzJkYjk3MzMyX3Ryb2xsMS5qcGciXSxbInAiLCJ0aHVtYiIsIngzOTA-Il0sWyJwIiwiY29udmVydCIsIi1xdWFsaXR5IDgxIC1hdXRvLW9yaWVudCJdXQ/troll1.jpg"),
-                                    4: URL(string:"https://c8.alamy.com/comp/CPB8PP/fremont-rocket-seattle-CPB8PP.jpg"),
-                                    5: URL(string:"https://fremont.com/wp-content/uploads/2017/07/lenin1-1050x700.jpg")]
+                                    4: URL(string:"https://fastly.4sqi.net/img/general/600x600/287485_W1Uhdz8WrqdxaOX61hEN1Kl6oE3c6FU1gn4ezc4qkFI.jpg"),
+                                    5: URL(string:"https://wheresmybackpack.files.wordpress.com/2014/02/lenin-fremont.jpg")]
 }
